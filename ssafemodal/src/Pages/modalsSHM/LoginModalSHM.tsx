1137614727205f80f29326657a6036390c7d6fbb
mung96 <mung5079@gmail.com>
2024-03-19 19:37:11 +0900

    feat: 로그인시 마이스페이스로 이동 구현

--- ssafemodal/src/Pages/modalsSHM/LoginModalSHM.tsx
+++ ssafemodal/src/Pages/modalsSHM/LoginModalSHM.tsx
@@ -1,4 +1,4 @@
-import { NicknameConsumer, NicknameProvider} from "../contexts/NicknameContext";
+import { NicknameConsumer } from "../contexts/NicknameContext";
 import {
   ModalBlock,
   Modal,
@@ -7,37 +7,52 @@ import {
   ModalMain,
 } from "./ModalSHM.element";
 import { useInput } from "../../hooks/useInput";
+import { useNavigate } from "react-router-dom";
 
-interface Modal{
+interface Modal {
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-};
+}
 
-export const LoginModalSHM:React.FC<Modal> = ({setIsModalOpen}) => {
+export const LoginModalSHM: React.FC<Modal> = ({ setIsModalOpen }) => {
+  const navigate = useNavigate();
   const handleOutsideClick = () => {
     setIsModalOpen(false);
   };
- const [nickname,changeNickname] = useInput();
+  const handleCompleteBtn = (actions: {
+    setNickname: (newNickname: string) => void;
+  }) => {
+    navigate("/mySpace");
+    actions.setNickname(nickname[0]);
+  };
+  const [nickname, changeNickname] = useInput();
   return (
     <>
-      <ModalBlock onClick={handleOutsideClick }></ModalBlock>
+      <ModalBlock onClick={handleOutsideClick}></ModalBlock>
       <Modal>
         <ModalHeader>
           <h2>닉네임을 입력하세요.</h2>
           <span>닉네임은 최소 4글자 이상이어야 합니다.</span>
         </ModalHeader>
         <ModalMain>
-          <input onChange={changeNickname}/>
+          <input onChange={changeNickname} />
         </ModalMain>
         <ModalFooter>
-        <NicknameConsumer>{
-          ({state,actions})=>{
-            return <button onClick={() => {actions.setNickname(nickname[0])}}>확인</button>}}
-        </NicknameConsumer>
+          <NicknameConsumer>
+            {({ state, actions }) => {
+              return (
+                <button
+                  onClick={() => handleCompleteBtn(actions)}
+                  // onClick={() => {
+                  //   actions.setNickname(nickname[0]);
+                  // }}
+                >
+                  확인
+                </button>
+              );
+            }}
+          </NicknameConsumer>
         </ModalFooter>
       </Modal>
     </>
   );
 };
-
-
-
