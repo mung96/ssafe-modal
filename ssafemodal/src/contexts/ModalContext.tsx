2d08ffb594d213db1bb66b4da46f2a1be2c6fd28
mung96 <mung5079@gmail.com>
2024-03-28 09:46:07 +0900

    feat:ModalContext구현

--- ssafemodal/src/contexts/ModalContext.tsx
+++ ssafemodal/src/contexts/ModalContext.tsx
@@ -1,82 +1,74 @@
-import { createContext } from "react";
+import {
+  createContext,
+  useState,
+  ComponentType,
+  HTMLAttributes,
+  PropsWithChildren,
+  useMemo,
+} from "react";
 import { IModal } from "../components/modals/Modal";
 
-export interface IModalState {
-  Component: IModal;
+//ComponentType말고 IModal로 바꿔보자.
+export type ModalComponent = ComponentType<any>;
+export type ModalProps = Record<string, any> | HTMLAttributes<HTMLDivElement>;
+
+export type ModalState = {
+  Component: ModalComponent;
+  props?: ModalProps;
   isOpen: boolean;
-}
+};
+
+type ModalsDispatch = {
+  open: (Component: ModalComponent, props: ModalProps) => void;
+  close: (Component: ModalComponent) => void;
+};
 
 //현재 open된 modal들을 나타냄
-export const ModalsStateContext = createContext<IModalState[]>([]);
+export const ModalsStateContext = createContext<ModalState[]>([]);
 
 //modal을 열고 닫는 함수
-export const ModalsDispatchContext = createContext({
+export const ModalsDispatchContext = createContext<ModalsDispatch>({
   open: () => {},
   close: () => {},
 });
 
-// import {
-//   ComponentType,
-//   useMemo,
-//   useState,
-//   createContext,
-//   PropsWithChildren,
-//   HTMLAttributes,
-// } from "react";
+const ModalsProvider = ({ children }: PropsWithChildren) => {
+  const [openedModals, setOpenedModals] = useState<ModalState[]>([]);
+  //매개변수로 받은 컴포넌트를 열어
+  const open = (Component: ModalComponent, props: ModalProps) => {
+    setOpenedModals((modals) => {
+      const modalIndex = modals.findIndex(
+        (modal) => modal.Component === Component
+      );
 
-// export type ModalsState = Array<{
-//   Component: ModalComponent;
-//   props?: ModalProps;
-//   isOpen: boolean;
-// }>;
-// export type ModalComponent = ComponentType;
-// export type ModalProps = Record<string, any> | HTMLAttributes<HTMLDivElement>;
+      if (modalIndex !== -1) {
+        //모달이 이미 배열에 있는 경우, 해당 모달의 isOpen값을 true로 변경
+        modals[modalIndex].isOpen = true;
+        modals[modalIndex].props = props;
+        return [...modals];
+      }
+      //없는 모달이었다면, 추가하고 열어
+      return [...modals, { Component, props, isOpen: true }];
+    });
+  };
+  const close = (Component: ModalComponent) => {
+    setOpenedModals((modals) => {
+      return modals.filter((modal) => {
+        return modal.Component !== Component;
+      });
+    });
+  };
 
-// interface ModalsDispatch {
-//   open: (Component: ModalComponent, props: ModalProps) => void;
-//   close: (Component: ModalComponent) => void;
-// }
+  const dispatch = useMemo(() => ({ open, close }), []);
 
-// export const ModalsStateContext = createContext<ModalsState>([]);
+  return (
+    <ModalsStateContext.Provider value={openedModals}>
+      <ModalsDispatchContext.Provider value={dispatch}>
+        {children}
+        {/* <Modals /> */}
+      </ModalsDispatchContext.Provider>
+    </ModalsStateContext.Provider>
+  );
+};
 
-// export const ModalsDispatchContext = createContext<ModalsDispatch>({
-//   open: () => {},
-//   close: () => {},
-// });
-
-// const ModalsProvider = ({ children }: PropsWithChildren) => {
-//   const [openedModals, setOpenedModals] = useState<ModalsState>([]);
-
-//   const open = (Component: ModalComponent, props: ModalProps) => {
-//     setOpenedModals((modals) => {
-//       const modalIndex = modals.findIndex(
-//         (modal) => modal.Component === Component
-//       );
-//       if (modalIndex !== -1) {
-//         modals[modalIndex].isOpen = true;
-//         modals[modalIndex].props = props;
-//         return [...modals];
-//       }
-//       return [...modals, { Component, props, isOpen: true }];
-//     });
-//   };
-
-//   const close = (Component: ModalComponent) => {
-//     setOpenedModals((modals) =>
-//       modals.map((modal) =>
-//         modal.Component === Component ? { ...modal, isOpen: false } : modal
-//       )
-//     );
-//   };
-
-//   const dispatch = useMemo(() => ({ open, close }), []);
-
-//   return (
-//     <ModalsStateContext.Provider value={openedModals}>
-//       <ModalsDispatchContext.Provider value={dispatch}>
-//         {children}
-//       </ModalsDispatchContext.Provider>
-//     </ModalsStateContext.Provider>
-//   );
-// };
-// export default ModalsProvider;
+export default ModalsProvider;
--- ssafemodal/src/index.tsx
+++ ssafemodal/src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ModalsProvider from "./contexts/ModalContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ModalsProvider>
+      <App />
+    </ModalsProvider>
   </React.StrictMode>
 );
